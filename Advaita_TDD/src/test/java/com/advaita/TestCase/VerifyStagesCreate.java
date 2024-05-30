afcabfb6982c69c4b78ee162228dc775a2b43095
W2129 <W2129@BLLGOPALREDDY.WYZMINDZ.COM>
2024-05-30 16:07:06 +0530

    yes

--- Advaita_TDD/src/test/java/com/advaita/TestCase/VerifyStagesCreate.java
+++ Advaita_TDD/src/test/java/com/advaita/TestCase/VerifyStagesCreate.java
@@ -25,63 +25,63 @@ import Advaita_TDD.Advaita_TDD.FakeData;
 
 public class VerifyStagesCreate extends TestBase {
 
-    FakeData fake = new FakeData();
+	FakeData fake = new FakeData();
 
-    public String processName = "P " + fake.lastName1();
-    public String processDesc = "P Desc";
-    public String subProcessName = "SP" + fake.lastName1();
-    public String subProcessDesc = "SP Desc";
-    public String subSubProcessName = "SSP " + fake.lastName1();
-    public String subSubProcessDesc = "SSP Desc";
+	public String processName = "P " + fake.lastName1();
+	public String processDesc = "P Desc";
+	public String subProcessName = "SP" + fake.lastName1();
+	public String subProcessDesc = "SP Desc";
+	public String subSubProcessName = "SSP " + fake.lastName1();
+	public String subSubProcessDesc = "SSP Desc";
 
-    public String metaDataName = "Test " + fake.lastName1();
+	public String metaDataName = "Test " + fake.lastName1();
 
-    public ExtentReports reports;
-    public ExtentSparkReporter htmlReporter;
-    public ExtentTest test;
+	public ExtentReports reports;
+	public ExtentSparkReporter htmlReporter;
+	public ExtentTest test;
 
-    LoginPage loginPage;
-    HomePage homePage;
+	LoginPage loginPage;
+	HomePage homePage;
 
-    Process process;
-    DataSet dataset;
-    MetaData metaData;
+	Process process;
+	DataSet dataset;
+	MetaData metaData;
 
-    Stages stages;
+	Stages stages;
 
-    public VerifyStagesCreate() {
-        super();
-    }
+	public VerifyStagesCreate() {
+		super();
+	}
 
-    @BeforeTest
-    public void setUp() throws Throwable {
-        initialization();
-        loginPage = new LoginPage();
-        homePage = loginPage.login("Capture_admin", "Qwerty@123");
+	@BeforeTest
+	public void setUp() throws Throwable {
+		initialization();
+		loginPage = new LoginPage();
+		homePage = loginPage.login("Capture_admin", "Qwerty@123");
 
-        htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir")+"/Reports/Stages extentReport.html");
-        reports = new ExtentReports();
-        reports.attachReporter(htmlReporter);
+		htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/Reports/Stages extentReport.html");
+		reports = new ExtentReports();
+		reports.attachReporter(htmlReporter);
 
-        // add environment details
-        reports.setSystemInfo("Machine", "Testpc1");
-        reports.setSystemInfo("OS", "Windows 11");
-        reports.setSystemInfo("user", "Gopal");
-        reports.setSystemInfo("Browser", "Chrome");
+		// add environment details
+		reports.setSystemInfo("Machine", "Testpc1");
+		reports.setSystemInfo("OS", "Windows 11");
+		reports.setSystemInfo("user", "Gopal");
+		reports.setSystemInfo("Browser", "Chrome");
 
-        // Configuration to change look and feel
-        htmlReporter.config().setDocumentTitle("Extent Report for Stages");
-        htmlReporter.config().setReportName("TEST Advaita");
-        htmlReporter.config().setTheme(Theme.STANDARD);
-        htmlReporter.config().setTimelineEnabled(true);
-        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+		// Configuration to change look and feel
+		htmlReporter.config().setDocumentTitle("Extent Report for Stages");
+		htmlReporter.config().setReportName("TEST Advaita");
+		htmlReporter.config().setTheme(Theme.STANDARD);
+		htmlReporter.config().setTimelineEnabled(true);
+		htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
 
-        process = new Process();
-        dataset = new DataSet();
-        metaData = new MetaData();
+		process = new Process();
+		dataset = new DataSet();
+		metaData = new MetaData();
 
-        stages = new Stages();
-    }
+		stages = new Stages();
+	}
 
 //    @Test(priority = 1)
 //    public void verifyProcessCreate() throws Throwable {
@@ -105,92 +105,90 @@ public class VerifyStagesCreate extends TestBase {
 //        metaData.createMetaData(metaDataName);
 //    }
 
-    @Test(priority = 4)
-    public void navigateFetchRecord() throws Throwable {
-        test = reports.createTest("clickOnworkflowDesign");
-        homePage.clickOnworkflowDesign();
+	@Test(priority = 4)
+	public void navigateFetchRecord() throws Throwable {
+		test = reports.createTest("clickOnworkflowDesign");
+		homePage.clickOnworkflowDesign();
 
-        test = reports.createTest("navigateFetchProcessRecord");
-        stages.navigateFetchProcessRecord();
+		test = reports.createTest("navigateFetchProcessRecord");
+		stages.navigateFetchProcessRecord();
 
-        test = reports.createTest("navigateFetchSubProcessRecord");
-        stages.navigateFetchSubProcessRecord();
+		test = reports.createTest("navigateFetchSubProcessRecord");
+		stages.navigateFetchSubProcessRecord();
 
-        test = reports.createTest("navigateFetchSubSubProcessRecord");
-        stages.navigateFetchSubSubProcessRecord();
+		test = reports.createTest("navigateFetchSubSubProcessRecord");
+		stages.navigateFetchSubSubProcessRecord();
 
-        test = reports.createTest("navigateFetchMetaDataRecord");
-        stages.navigateFetchMetaDataRecord();
-    }
+		test = reports.createTest("navigateFetchMetaDataRecord");
+		stages.navigateFetchMetaDataRecord();
+	}
 
-    @Test(priority = 5)
-    public void VerifyStagesTabIsDisplayed() throws Throwable {
-        test = reports.createTest("VerifyStagesTabIsDisplayed");
-        stages.VerifyStagesTabIsDisplayed();
-    }
+	@Test(priority = 5)
+	public void VerifyStagesTabIsDisplayed() throws Throwable {
+		test = reports.createTest("VerifyStagesTabIsDisplayed");
+		stages.VerifyStagesTabIsDisplayed();
+	}
 
-    @Test(priority = 6)
-    public void verifyCreateStagesButton() throws Throwable {
-        test = reports.createTest("verifyCreateStagesButton");
-        stages.verifyCreateStagesButton();
-    }
+	@Test(priority = 6)
+	public void verifyCreateStagesButton() throws Throwable {
+		test = reports.createTest("verifyCreateStagesButton");
+		stages.verifyCreateStagesButton();
+	}
 
-    @Test(priority = 7)
-    public void verifyStageNameTextBox() throws Throwable {
-        test = reports.createTest("verifyStageNameTextBox");
-        stages.verifyStageNameTextBox(fake.firstCapString());
-    }
+	@Test(priority = 7)
+	public void verifyStageNameTextBox() throws Throwable {
+		test = reports.createTest("verifyStageNameTextBox");
+		stages.verifyStageNameTextBox(fake.firstCapString());
+	}
 
-    @Test(priority = 8)
-    public void verifyStageSelectProcessDropDown() throws Throwable {
-        test = reports.createTest("verifyStageSelectProcessDropDown");
-        stages.verifyStageSelectProcessDropDown();
-    }
+	@Test(priority = 8)
+	public void verifyStageSelectProcessDropDown() throws Throwable {
+		test = reports.createTest("verifyStageSelectProcessDropDown");
+		stages.verifyStageSelectProcessDropDown();
+	}
 
-    @Test(priority = 10)
-    public void verifyStageSelectSubProcessDropDown() throws Throwable {
-        test = reports.createTest("verifyStageSelectSubProcessDropDown");
-        stages.verifyStageSelectSubProcessDropDown();
-    }
+	@Test(priority = 10)
+	public void verifyStageSelectSubProcessDropDown() throws Throwable {
+		test = reports.createTest("verifyStageSelectSubProcessDropDown");
+		stages.verifyStageSelectSubProcessDropDown();
+	}
 
-    @Test(priority = 11)
-    public void verifyStageSelectSubSubProcessDropDown() throws Throwable {
-        test = reports.createTest("verifyStageSelectSubSubProcessDropDown");
-        stages.verifyStageSelectSubSubProcessDropDown();
-    }
+	@Test(priority = 11)
+	public void verifyStageSelectSubSubProcessDropDown() throws Throwable {
+		test = reports.createTest("verifyStageSelectSubSubProcessDropDown");
+		stages.verifyStageSelectSubSubProcessDropDown();
+	}
 
-    @Test(priority = 12)
-    public void verifyStageCalculationTypeDropDown() throws Throwable {
-        test = reports.createTest("verifyStageCalculationTypeDropDown");
-        stages.verifyStageCalculationTypeDropDown();
-    }
+	@Test(priority = 12)
+	public void verifyStageCalculationTypeDropDown() throws Throwable {
+		test = reports.createTest("verifyStageCalculationTypeDropDown");
+		stages.verifyStageCalculationTypeDropDown();
+	}
 
-    @Test(priority = 13)
-    public void verifyAddSectionA() throws Throwable {
-        test = reports.createTest("verifyAddSectionA");
-        stages.verifyAddSectionA();
-    }
+	@Test(priority = 13)
+	public void verifyAddSectionA() throws Throwable {
+		test = reports.createTest("verifyAddSectionA");
+		stages.verifyAddSectionA();
+	}
 
- 
+	@AfterMethod
+	public void getResult(ITestResult result) throws IOException, Throwable {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			// Mark the test as failed in the ExtentReports
+			test.fail(result.getThrowable());
+			// Add screenshot to ExtentReports
+			String screenshotPath = ScreenShorts.captureScreenshot(result.getMethod().getMethodName());
+			test.addScreenCaptureFromPath(screenshotPath);
+		}
+		// Close ExtentReports
 
-    @AfterMethod
-    public void getResult(ITestResult result) throws IOException, Throwable {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            // Mark the test as failed in the ExtentReports
-            test.fail(result.getThrowable());
-            // Add screenshot to ExtentReports
-            String screenshotPath = ScreenShorts.captureScreenshot(result.getMethod().getMethodName());
-            test.addScreenCaptureFromPath(screenshotPath);
-        }
-        // Close ExtentReports
-        
-        reports.flush();
-    }
+		reports.flush();
+	}
 
-    @AfterTest
-    public void tearDown() {
-        driver.manage().window().minimize();
-        driver.quit();
-        reports.flush();
-    }
+	@AfterTest
+	public void tearDown() {
+		driver.manage().window().minimize();
+		driver.quit();
+		reports.flush();
+	}
 }
