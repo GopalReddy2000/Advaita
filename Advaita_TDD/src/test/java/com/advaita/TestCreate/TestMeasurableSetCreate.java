168b47be5c5f1a93665c4e896ad368f1747ebf94
W2129 <W2129@BLLGOPALREDDY.WYZMINDZ.COM>
2024-06-04 11:17:40 +0530

    Merge branch 'master' of https://github.com/GopalReddy2000/Advaita.git

---
+++ Advaita_TDD/src/test/java/com/advaita/TestCreate/TestMeasurableSetCreate.java
@@ -0,0 +1,82 @@
+package com.advaita.TestCreate;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.advaita.BaseClass.TestBase;
+import com.advaita.Login.Home.HomePage;
+import com.advaita.Login.Home.LoginPage;
+import com.advaita.WorkFlowDesign.PageObject.MasterParameterMeasurableSetPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class TestMeasurableSetCreate extends TestBase {
+
+	static int createCount = 1;
+
+//	Faker faker = new Faker();
+//		public String num = "24";
+//	public String questionSet = faker.name().lastName() + " TestAudit";
+
+	public ExtentReports reports;
+	public ExtentSparkReporter htmlReporter;
+	public ExtentTest test;
+
+	LoginPage loginPage;
+	HomePage homePage;
+
+	MasterParameterMeasurableSetPage measurableSetPage;
+
+	public TestMeasurableSetCreate() {
+		super();
+	}
+
+	@BeforeTest
+	public void setUp() throws Throwable {
+
+		initialization();
+		loginPage = new LoginPage();
+		homePage = loginPage.login("Capture_admin", "Qwerty@123");
+
+		htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/Reports/MeasurableCreate.html");
+		reports = new ExtentReports();
+		reports.attachReporter(htmlReporter);
+
+		// add environment details
+		reports.setSystemInfo("Machine", "Testpc1");
+		reports.setSystemInfo("OS", "Windows 11");
+		reports.setSystemInfo("user", "Gopal");
+		reports.setSystemInfo("Browser", "Chrome");
+
+		// Configuration to change look and feel
+		htmlReporter.config().setDocumentTitle("Extent Report for MeasurableSetPage");
+		htmlReporter.config().setReportName("TEST Advaita");
+		htmlReporter.config().setTheme(Theme.STANDARD);
+		htmlReporter.config().setTimelineEnabled(true);
+		htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+
+		measurableSetPage = new MasterParameterMeasurableSetPage();
+	}
+
+	@Test(priority = 1)
+	public void verifyMeasurableSetCreatePage() throws Throwable {
+		
+		test = reports.createTest("verifyMeasurableSetCreatePage");
+		homePage.clickOnworkflowDesign();
+		measurableSetPage.createMeasurableSet(createCount);
+		
+	}
+
+	@AfterTest
+	public void tearDown() {
+
+		driver.manage().window().minimize();                                       
+		driver.quit();
+		reports.flush();
+
+	}
+
+}
